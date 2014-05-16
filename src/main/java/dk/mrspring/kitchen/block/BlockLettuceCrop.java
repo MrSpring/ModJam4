edf05d9592e6317bfb727f71279d1c255ea37ec8
Konrad <MrSping@MrSpring.dk>
2014-05-16 23:31:15 +0200

    Commit #29

--- src/main/java/dk/mrspring/kitchen/block/BlockLettuceCrop.java
+++ src/main/java/dk/mrspring/kitchen/block/BlockLettuceCrop.java
@@ -15,6 +15,7 @@ import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
+import dk.mrspring.kitchen.Kitchen;
 import dk.mrspring.kitchen.KitchenItems;
 import dk.mrspring.kitchen.ModInfo;
 
@@ -24,7 +25,46 @@ public class BlockLettuceCrop extends BlockCrops
 	
 	public BlockLettuceCrop()
 	{
+		super();
+		
 		this.setBlockName("lettuce_crop");
 		this.setBlockTextureName(ModInfo.modid + ":lettuce_crop");
+		
+		this.setCreativeTab(Kitchen.instance.tab);
+	}
+	
+	@Override
+	public IIcon getIcon(int side, int metadata)
+	{
+		if (metadata < 7)
+		{
+			if (metadata == 6)
+				metadata = 5;
+			
+			return this.icons[metadata >> 1];
+		}
+		else
+			return this.icons[3];
+	}
+	
+	protected Item func_149866_i()
+	{
+		return KitchenItems.lettuce;
+	}
+	
+	protected Item func_149865_P()
+	{
+		return KitchenItems.lettuce;
+	}
+	
+	@Override
+	public void registerBlockIcons(IIconRegister p_149651_1_)
+	{
+		this.icons = new IIcon[4];
+		
+		for (int i = 0; i < this.icons.length; ++i)
+		{
+			this.icons[i] = p_149651_1_.registerIcon(this.getTextureName() + "_stage_" + i);
+		}
 	}
 }
