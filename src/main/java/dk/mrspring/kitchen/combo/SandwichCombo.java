4cf586c98661b61a8680eda7328482a03bf07450
Konrad <MrSping@MrSpring.dk>
2014-05-16 17:55:19 +0200

    Commit #24

---
+++ src/main/java/dk/mrspring/kitchen/combo/SandwichCombo.java
@@ -0,0 +1,40 @@
+package dk.mrspring.kitchen.combo;
+
+import java.util.ArrayList;
+
+import dk.mrspring.kitchen.item.ItemSandwichable;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+
+public class SandwichCombo
+{
+	protected ArrayList<ItemSandwichable> layers = new ArrayList<ItemSandwichable>();
+	// TODO Implement PotionEffect things
+	
+	public SandwichCombo(ItemStack[] items)
+	{
+		for (int i = 0; i < items.length; ++i)
+		{
+			if (items[i] != null)
+				this.layers.add((ItemSandwichable) items[i].getItem());
+		}
+	}
+	
+	public boolean matches(ItemStack sandwich)
+	{
+		ArrayList<ItemSandwichable> layersInSandwich = new ArrayList<ItemSandwichable>();
+		NBTTagList layersList = sandwich.stackTagCompound.getTagList("SandwichLayers", 10);
+		
+		for (int i = 0; i < layersList.tagCount(); ++i)
+		{
+			NBTTagCompound layerCompound = layersList.getCompoundTagAt(i);
+			layersInSandwich.add((ItemSandwichable) ItemStack.loadItemStackFromNBT(layerCompound).getItem());
+		}
+		
+		if (this.layers.containsAll(layersInSandwich) && layersInSandwich.containsAll(this.layers))
+			return true;
+		else
+			return false;
+	}
+}
