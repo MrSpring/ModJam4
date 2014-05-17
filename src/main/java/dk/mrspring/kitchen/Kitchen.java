8ff2fe10aa2540f6fa7b97c4a8726a2bc4dcb445
Konrad <MrSping@MrSpring.dk>
2014-05-18 01:23:15 +0200

    Commit #65

--- src/main/java/dk/mrspring/kitchen/Kitchen.java
+++ src/main/java/dk/mrspring/kitchen/Kitchen.java
@@ -7,15 +7,16 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import dk.mrspring.kitchen.block.BlockBase;
 import dk.mrspring.kitchen.item.ItemBase;
 import dk.mrspring.kitchen.tileentity.TileEntityBoard;
+import dk.mrspring.kitchen.world.gen.WorldGenWildLettuce;
 import dk.mrspring.kitchen.world.gen.WorldGenWildTomato;
 
 @Mod(modid = ModInfo.modid, name = ModInfo.name, version = ModInfo.version)
@@ -51,6 +52,7 @@ public class Kitchen
 	public static void init(FMLInitializationEvent event)
 	{
 		GameRegistry.registerWorldGenerator(new WorldGenWildTomato(), 0);
+		GameRegistry.registerWorldGenerator(new WorldGenWildLettuce(), 1);
 		
 		GameRegistry.addRecipe(new ShapedOreRecipe(KitchenBlocks.board, new Object[] { "SPS", Character.valueOf('S'), "slabWood", Character.valueOf('P'), Blocks.wooden_pressure_plate }));
 		GameRegistry.addRecipe(new ShapedOreRecipe(KitchenItems.knife, new Object[] { "I ", " S", Character.valueOf('S'), "stickWood", Character.valueOf('I'), Items.iron_ingot }));
--- src/main/java/dk/mrspring/kitchen/block/BlockBoard.java
+++ src/main/java/dk/mrspring/kitchen/block/BlockBoard.java
@@ -1,6 +1,7 @@
 package dk.mrspring.kitchen.block;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
@@ -15,6 +16,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.StatCollector;
@@ -39,6 +41,7 @@ public class BlockBoard extends BlockContainer
 		
 		this.setBlockName("board");
 		this.setBlockTextureName("minecraft:planks_oak");
+		this.setTickRandomly(true);
 		
 		this.setCreativeTab(Kitchen.instance.tab);
 		
@@ -127,6 +130,26 @@ public class BlockBoard extends BlockContainer
 		}
 	}
 	
+	@Override
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World p_149668_1_, int p_149668_2_, int p_149668_3_, int p_149668_4_)
+	{
+		return null;
+	}
+	
+	@Override
+	public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune)
+	{
+		TileEntityBoard tEntity = (TileEntityBoard) world.getTileEntity(x, y, z);
+		
+		ArrayList<ItemStack> toReturn = new ArrayList<ItemStack>();
+		
+		for (int i = 0; i < tEntity.getLayers().length && tEntity.getLayers()[i] != null; ++i)
+		{
+			toReturn.add(tEntity.getLayers()[i]);
+		}
+		
+		return toReturn;
+	}
 	
 	@Override
 	public void setBlockBoundsBasedOnState(IBlockAccess world, int x, int y, int z)
