bbfa90d4c2064709488ae362c7b81b1c52b794d3
Konrad Rasmussen <MrSpring@mrspring.dk>
2014-08-29 15:19:00 +0200

    More work on rendering

--- src/main/java/dk/mrspring/kitchen/tileentity/TileEntityBoardSpecialRenderer.java
+++ src/main/java/dk/mrspring/kitchen/tileentity/TileEntityBoardSpecialRenderer.java
@@ -17,12 +17,12 @@ import java.util.List;
 public class TileEntityBoardSpecialRenderer extends TileEntitySpecialRenderer
 {
 	private List<ItemStack> layers;
-	private double yItemOffset = 0.0D;
+	private int yItemOffset = 0;
 	
 	@Override
 	public void renderTileEntityAt(TileEntity var1, double x, double y, double z, float var8)
 	{
-		this.yItemOffset = 0.0D;
+		this.yItemOffset = 0;
 		
 		GL11.glPushMatrix();
 		
@@ -31,12 +31,21 @@ public class TileEntityBoardSpecialRenderer extends TileEntitySpecialRenderer
 		TileEntityBoard tileEntity = (TileEntityBoard) var1;
 		
 		this.layers = tileEntity.getAllItems();
-		
-		for (int i = 0; i < this.layers.length; ++i)
+
+		for (ItemStack itemStack : this.layers)
 		{
-			if (this.layers[i] != null)
-				renderItem(this.layers[i], 0.5D, 0.145D + (i * 0.033F + this.yItemOffset), 0.33D, i);
+			if (itemStack != null)
+				if (itemStack.getItem() != null)
+				{
+
+				}
 		}
+
+		/*for (int i = 0; i < this.layers.size(); ++i)
+		{
+			if (this.layers.get(i) != null)
+				renderItem(this.layers[i], 0.5D, 0.145D + (i * 0.033F + this.yItemOffset), 0.33D, i);
+		}*/
 		
 		GL11.glPopMatrix();
 	}
@@ -56,22 +65,15 @@ public class TileEntityBoardSpecialRenderer extends TileEntitySpecialRenderer
 				else
 				{ ((ISandwichable) this.layers[i].getItem()).getTopModel().render(Minecraft.getMinecraft().renderViewEntity, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F); this.yItemOffset += (((ISandwichable) this.layers[i].getItem()).modelTopHeight * 0.03D); }
 			else*/
-			{
+			/*{
 				EntityItem itemEntity = new EntityItem(Minecraft.getMinecraft().thePlayer.getEntityWorld(), 0D, 0D, 0D, item);
 				itemEntity.hoverStart = 0.0F;
 				RenderItem.renderInFrame = true;
 				GL11.glRotatef(180, 0, 1, 1);
 				RenderManager.instance.renderEntityWithPosYaw(itemEntity, 0.0D, 0.0D, 0.0D, 0.0F, 0.0F);
 				RenderItem.renderInFrame = false;
-			}
+			}*/
 			
 		GL11.glPopMatrix();
 	}
-
-    enum RenderType
-    {
-        DEFAULT,
-        SPECIAL,
-        SIMPLE_MODEL
-    }
 }
